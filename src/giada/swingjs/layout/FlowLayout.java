d5fc952be0cc2a47620eab46ea6f13200beeebd1
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-04-10 19:40:08 +0200

    flowlayout

---
+++ src/giada/swingjs/layout/FlowLayout.java
@@ -0,0 +1,16 @@
+package giada.swingjs.layout;
+
+/**
+ *
+ * @author gianpiero.diblasi
+ */
+public class FlowLayout {
+
+  public static final int LEFT = 0;
+  public static final int CENTER = 1;
+  public static final int RIGHT = 2;
+  public static final int LEADING = 3;
+
+  public FlowLayout() {
+  }
+}
