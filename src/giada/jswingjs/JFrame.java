4534230578a991c973c466e99f49d352c0caa4f1
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-04-10 15:30:19 +0200

    first commit

--- src/giada/jswingjs/JFrame.java
+++ src/giada/jswingjs/JFrame.java
@@ -1,9 +1,18 @@
 package giada.jswingjs;
 
+import static def.dom.Globals.document;
+import def.dom.HTMLElement;
+
 /**
  * The javax.swing.JFrame clone
+ *
  * @author gianpiero.diblasi
  */
 public class JFrame extends JComponent {
-  
+
+  public JFrame() {
+    HTMLElement div = document.createElement("div");
+    div.classList.add("jframe");
+    document.querySelector("body").appendChild(div);
+  }
 }
--- transpile/giada/jswingjs/JFrame.js
+++ transpile/giada/jswingjs/JFrame.js
@@ -1,6 +1,13 @@
 /**
  * The javax.swing.JFrame clone
+ *
  * @author gianpiero.diblasi
  */
 class JFrame extends JComponent {
+
+  constructor() {
+    let div = document.createElement("div");
+    div.classList.add("jframe");
+    document.querySelector("body").appendChild(div);
+  }
 }
