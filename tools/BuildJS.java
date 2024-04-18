0f8654cf8a86412ef714713720b03eed2a249c3f
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-04-18 18:01:39 +0200

    total refactoring

--- tools/BuildJS.java
+++ tools/BuildJS.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 public class BuildJS {
 
-  private final static List<String> toRename = List.of("Button", "CheckBox", "ComboBox", "Component", "Frame", "Label", "Panel", "RadioButton", "Spinner", "ToggleButton");
+  private final static List<String> toRename = List.of("Button", "CheckBox", "ComboBox", "Component", "Frame", "Label", "Panel", "RadioButton", "Slider", "Spinner", "ToggleButton");
 
   @SuppressWarnings({"UseOfSystemOutOrSystemErr", "CallToPrintStackTrace", "MismatchedQueryAndUpdateOfCollection"})
   private static void watch(File swingjs, File in, File out, boolean findParent, boolean rename) throws Exception {
