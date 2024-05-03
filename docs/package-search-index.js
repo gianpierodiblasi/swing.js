b435cb72553c44c23b9378c2bccc9b51b1b5a344
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-05-03 18:11:48 +0200

    colorchooser

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"javascript"},{"l":"javascript.awt"},{"l":"javascript.awt.event"},{"l":"javascript.swing"},{"l":"javascript.swing.event"},{"l":"javascript.swing.MnR"},{"l":"javascript.util"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"javascript"},{"l":"javascript.awt"},{"l":"javascript.awt.event"},{"l":"javascript.swing"},{"l":"javascript.swing.colorchooser"},{"l":"javascript.swing.event"},{"l":"javascript.swing.MnR"},{"l":"javascript.util"}]
