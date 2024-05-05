b88d54f0c127b3b3e9d579622ba784c3080b8085
gianpiero.diblasi <gianpiero.diblasi@192.168.1.145>
2024-05-05 13:52:11 +0200

    colorchooser

--- src/javascript/util/Translations.java
+++ src/javascript/util/Translations.java
@@ -31,42 +31,50 @@ public class Translations {
     switch (navigator.language.substring(0, 2)) {
       case "en":
       default:
-        Translations.JSOptionPane_OK = "OK";
-        Translations.JSOptionPane_YES = "Yes";
-        Translations.JSOptionPane_NO = "No";
-        Translations.JSOptionPane_CANCEL = "Cancel";
-        Translations.JSColorChooser_HUE = "Hue";
-        Translations.JSColorChooser_SATURATION = "Saturation";
-        Translations.JSColorChooser_VALUE = "Value";
-        Translations.JSColorChooser_LIGHTNESS = "Lightness";
-        Translations.JSColorChooser_RED = "Red";
-        Translations.JSColorChooser_GREEN = "Green";
-        Translations.JSColorChooser_BLUE = "Blue";
-        Translations.JSColorChooser_CYAN = "Cyan";
-        Translations.JSColorChooser_MAGENTA = "Magenta";
-        Translations.JSColorChooser_YELLOW = "Yellow";
-        Translations.JSColorChooser_BLACK = "Black";
+        Translations.setEnglish();
         break;
       case "it":
-        Translations.JSOptionPane_OK = "OK";
-        Translations.JSOptionPane_YES = "Si";
-        Translations.JSOptionPane_NO = "No";
-        Translations.JSOptionPane_CANCEL = "Annulla";
-        Translations.JSColorChooser_HUE = "Tonalit\u00E0";
-        Translations.JSColorChooser_SATURATION = "Saturazione";
-        Translations.JSColorChooser_VALUE = "Valore";
-        Translations.JSColorChooser_LIGHTNESS = "Luminosit\u00E0";
-        Translations.JSColorChooser_RED = "Rosso";
-        Translations.JSColorChooser_GREEN = "Verde";
-        Translations.JSColorChooser_BLUE = "Blu";
-        Translations.JSColorChooser_CYAN = "Ciano";
-        Translations.JSColorChooser_MAGENTA = "Magenta";
-        Translations.JSColorChooser_YELLOW = "Giallo";
-        Translations.JSColorChooser_BLACK = "Nero";
+        Translations.setItalian();
         break;
     }
   }
 
   private Translations() {
+  }
+
+  public static void setEnglish() {
+    Translations.JSOptionPane_OK = "OK";
+    Translations.JSOptionPane_YES = "Yes";
+    Translations.JSOptionPane_NO = "No";
+    Translations.JSOptionPane_CANCEL = "Cancel";
+    Translations.JSColorChooser_HUE = "Hue";
+    Translations.JSColorChooser_SATURATION = "Saturation";
+    Translations.JSColorChooser_VALUE = "Value";
+    Translations.JSColorChooser_LIGHTNESS = "Lightness";
+    Translations.JSColorChooser_RED = "Red";
+    Translations.JSColorChooser_GREEN = "Green";
+    Translations.JSColorChooser_BLUE = "Blue";
+    Translations.JSColorChooser_CYAN = "Cyan";
+    Translations.JSColorChooser_MAGENTA = "Magenta";
+    Translations.JSColorChooser_YELLOW = "Yellow";
+    Translations.JSColorChooser_BLACK = "Black";
+  }
+
+  public static void setItalian() {
+    Translations.JSOptionPane_OK = "OK";
+    Translations.JSOptionPane_YES = "Si";
+    Translations.JSOptionPane_NO = "No";
+    Translations.JSOptionPane_CANCEL = "Annulla";
+    Translations.JSColorChooser_HUE = "Tonalit\u00E0";
+    Translations.JSColorChooser_SATURATION = "Saturazione";
+    Translations.JSColorChooser_VALUE = "Valore";
+    Translations.JSColorChooser_LIGHTNESS = "Luminosit\u00E0";
+    Translations.JSColorChooser_RED = "Rosso";
+    Translations.JSColorChooser_GREEN = "Verde";
+    Translations.JSColorChooser_BLUE = "Blu";
+    Translations.JSColorChooser_CYAN = "Ciano";
+    Translations.JSColorChooser_MAGENTA = "Magenta";
+    Translations.JSColorChooser_YELLOW = "Giallo";
+    Translations.JSColorChooser_BLACK = "Nero";
   }
 }
